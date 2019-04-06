74ea372243291429a0155f606fab59dcb201aba9
Dmytro Uchkin <demjan@Demjans-MacBook-Pro.local>
2019-04-06 17:08:58 +0300

    Routing/Signup components location change;

---
+++ Frontend/src/app/organization/signup/signup.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  @ViewChild('volunteerForm') volunteerForm;
+  model: any = {};
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  submit() {
+    console.log(JSON.stringify(this.volunteerForm.value, null, 2));
+  }
+}
