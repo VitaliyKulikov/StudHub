9ef9a82d0074f4a66e9d00bb7f3e3620c592a1c9
yfozekosh <yfozekosh@gmail.com>
2019-04-06 17:12:18 +0300

    add password to signup

--- Frontend/src/app/signin.component/signin/signin.component.ts
+++ Frontend/src/app/signin.component/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
-
+  @ViewChild('loginForm') loginForm;
   constructor() { }
 
   ngOnInit() {
   }
 
+  submit() {
+    console.log(JSON.stringify(this.loginForm.value, null, 2));
+  }
 }
