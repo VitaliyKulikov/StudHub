e98fd818b7adf2310a6bfa9b857cacc6bb342a91
Bogdan Rudka <rudka.bogda@gmail.com>
2019-04-06 16:32:57 +0300

    setup spring data rest

--- src/main/java/org/hackathon/repository/PrincipalRepository.java
+++ src/main/java/org/hackathon/repository/PrincipalRepository.java
@@ -3,26 +3,15 @@ package org.hackathon.repository;
 import org.hackathon.entity.Principal;
 import org.hackathon.mapper.PrincipalMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.List;
 
-@Repository
-public class PrincipalRepository {
-    private DataSource dataSource;
-    private PrincipalMapper mapper;
-
-    @Autowired
-    public PrincipalRepository(DataSource dataSource, PrincipalMapper mapper) {
-        this.dataSource = dataSource;
-        this.mapper = mapper;
-    }
-
-    public List<Principal> getAllUsers(){
-        return new JdbcTemplate(dataSource)
-                .query("SELECT * FROM PRINCIPAL", mapper);
-
-    }
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface PrincipalRepository extends PagingAndSortingRepository<Principal, Integer> {
 }
