4a445c222217f886cf5584802f29743583d2e6e6
yfozekosh <yfozekosh@gmail.com>
2019-04-07 00:04:53 +0300

    add signin volonterr

--- Frontend/src/app/volunteer/signup/signup.component.ts
+++ Frontend/src/app/volunteer/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {AuthService} from '../../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -10,7 +11,7 @@ export class SignupComponent implements OnInit {
   @ViewChild('volunteerForm') volunteerForm;
   isLoading = false;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   ngOnInit() {
@@ -20,6 +21,7 @@ export class SignupComponent implements OnInit {
     this.isLoading = true;
     try {
       await this.authService.signin(this.volunteerForm.value).toPromise();
+      this.router.navigate(['signin']);
     } finally {
       this.isLoading = false;
     }
