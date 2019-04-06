cb865339ab1ade66fa5b08c0fcced01c1ab959ea
emiliia.nesterovych <emiliia.nesterovych@gmail.com>
2019-04-06 14:21:28 +0300

    Create common dtos

---
+++ src/main/java/org/hackathon/entity/Organisation.java
@@ -0,0 +1,68 @@
+package org.hackathon.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Organisation {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "description", length = 1500)
+    private String description;
+
+    @Column(name = "image")
+    private byte[] image;
+
+    @ManyToMany
+    private List<Principal> members;
+
+    public Organisation() {
+    }
+
+    public Organisation(int id, String name, String description, byte[] image) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
--- src/main/java/org/hackathon/entity/Principal.java
+++ src/main/java/org/hackathon/entity/Principal.java
@@ -1,25 +1,162 @@
 package org.hackathon.entity;
 
-public class Principal {
+import org.hackathon.security.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Entity
+public class Principal implements UserDetails {
+    @Id
     private int id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 100)
     private String username;
+
+    @Column(name = "password", nullable = false, length = 50)
     private String password;
 
-    public Principal(int id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
+    @Column(name = "email", unique = true, nullable = false, length = 300)
+    private String email;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String surname;
+
+    @Column(name = "image")
+    private byte[] image;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "address", length = 300)
+    private String address;
+
+    @Enumerated
+    private Role role;
+
+    @ManyToMany
+    private List<Organisation> organisations;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
     }
 
     public int getId() {
         return id;
     }
 
-    public String getUsername() {
-        return username;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getPassword() {
-        return password;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public List<Organisation> getOrganisations() {
+        return organisations;
+    }
+
+    public void setOrganisations(List<Organisation> organisations) {
+        this.organisations = organisations;
     }
 }
