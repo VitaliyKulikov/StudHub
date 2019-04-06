12bef7785aced7c32aa8217d18f4beb68adb99e1
yfozekosh <yfozekosh@gmail.com>
2019-04-06 15:28:32 +0300

    Add error toasts on 400+ status

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,6 +6,12 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {TransferHttpCacheModule} from '@nguniversal/common';
 import {SignupComponent} from './signup.component/signup/signup.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CommonModule} from '@angular/common';
+import {ToastrModule} from 'ngx-toastr';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ErrorInterceptor} from '../interceptors/error.interceptor';
+import {ErrorSerivce} from '../services/error.service';
 import {SigninComponent} from './signin.component/signin/signin.component';
 
 @NgModule({
@@ -25,8 +31,20 @@ import {SigninComponent} from './signin.component/signin/signin.component';
       { path: 'signin', component: SigninComponent, pathMatch: 'full' },
     ]),
     TransferHttpCacheModule,
+    CommonModule,
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot()
   ],
-  providers: [],
+    providers: [
+        ErrorSerivce,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorInterceptor,
+            multi: true
+        }
+    ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
