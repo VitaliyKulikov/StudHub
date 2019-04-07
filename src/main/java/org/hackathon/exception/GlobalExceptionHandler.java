35a6a15415ce4586a25f555369c2bc461c3b9ab6
Bogdan Rudka <rudka.bogda@gmail.com>
2019-04-07 13:21:06 +0300

    Fix exception handling

--- src/main/java/org/hackathon/exception/GlobalExceptionHandler.java
+++ src/main/java/org/hackathon/exception/GlobalExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestControllerAdvice
+//@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = IllegalArgumentException.class)
